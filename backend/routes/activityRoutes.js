1f070333f9985807211c88b45030a4e4ea668280
Nirmala <nirmalawilujeng@gmail.com>
2026-05-19 01:32:59 +0700

    add authenticationtoken to log activity

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const activityController = require('../controllers/activityController');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
-router.post('/watch', activityController.recordWatch);
+router.post('/watch', authenticateToken, activityController.recordWatch);
 
 module.exports = router;
